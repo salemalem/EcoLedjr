f94d53330db3873f0cc37a36700ee40d9fd21b87
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-06-29 23:00:03 +0600

    Initial Blueprint of Blockchain

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,33 @@
 void main() {
   print('Hello, World!');
 }
+
+// following
+// https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
+
+class Blockchain {
+  List<dynamic> current_transactions;
+  List<dynamic> chain_of_transactions;
+
+  // constructor
+  Blockchain() {
+    current_transactions = [];
+    chain_of_transactions = [];
+  }
+
+  void new_block() {
+
+  }
+
+  void new_transaction() {
+
+  }
+
+  static String hash(dynamic block) {
+
+  }
+
+  dynamic get last_block {
+
+  }
+}
