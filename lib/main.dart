fa67d1d04a8c4f234369465955c3ea356c81975c
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-06-30 14:06:25 +0600

    pubspec yaml to track environment and packages added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:crypto/crypto.dart';
+import 'package:convert/convert.dart';
+import 'dart:convert';
+
 void main() {
   print('Hello, World!');
   Blockchain blockchain1 = Blockchain();
@@ -10,32 +14,36 @@ void main() {
 // https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
 
 class Blockchain {
-  List<Map> current_transactions;
+  List<Map> current_transactions; // pending transactions that should be stored on nodes
   List<Map> chain_of_transactions;
 
   // constructor
   Blockchain() {
     current_transactions = [];
     chain_of_transactions = [];
+
+    chain_of_transactions.add({"genesis": "first block"});
   }
 
   void new_block() {
 
   }
 
-  void new_transaction() {
+  int new_transaction() {
     this.current_transactions.add({
       'sender': 'Shyngys',
       'recipient': 'Abhi',
       'amount': '666',
     });
+
+    return this.chain_of_transactions.indexOf(this.chain_of_transactions.last) + 1;
   }
 
   static String hash(dynamic block) {
 
   }
 
-  dynamic get last_block {
-
+  Map get last_block {
+    return this.chain_of_transactions.last;
   }
 }
